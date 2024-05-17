c76f296f7889ef636c062cd94aff17d87bf70087
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2024-05-17 18:37:31 -0500

    feat: bus entity created

--- src/main/java/com/chapaTuBus/webService/planification/domain/model/entities/Bus.java
+++ src/main/java/com/chapaTuBus/webService/planification/domain/model/entities/Bus.java
@@ -1,4 +1,19 @@
 package com.chapaTuBus.webService.planification.domain.model.entities;
 
+import com.chapaTuBus.webService.planification.domain.model.valueobjects.BusModel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Bus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String licensePlate;
+
+    
+    private BusModel busModel;
 }
--- src/main/java/com/chapaTuBus/webService/planification/domain/model/valueobjects/BusModel.java
+++ src/main/java/com/chapaTuBus/webService/planification/domain/model/valueobjects/BusModel.java
@@ -1,4 +1,31 @@
 package com.chapaTuBus.webService.planification.domain.model.valueobjects;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
 public class BusModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String modelName;
+
+    private String manufacturer;
+
+    private int seatingCapacity;
+
+    private int totalCapacity;
+
+    private double lenght;
+
+    private double height;
+
+    private double width;
+
+    private int year;
+
 }
