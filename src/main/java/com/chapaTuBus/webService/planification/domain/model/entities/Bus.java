e2ab4596a39d6bc735fe5af6b2231905a8179ede
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2024-05-19 16:06:04 -0500

    feat: BusModel Class deleted

--- src/main/java/com/chapaTuBus/webService/planification/domain/model/entities/Bus.java
+++ src/main/java/com/chapaTuBus/webService/planification/domain/model/entities/Bus.java
@@ -1,6 +1,5 @@
 package com.chapaTuBus.webService.planification.domain.model.entities;
 
-import com.chapaTuBus.webService.planification.domain.model.valueobjects.BusModel;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -12,9 +11,6 @@ public class Bus {
     private Long id;
 
     private String licensePlate;
-
-    @ManyToOne
-    private BusModel busModel;
 
 
 }
