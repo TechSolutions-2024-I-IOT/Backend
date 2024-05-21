d42424ca01aef9801f86c57c1e3a33f2d09bd7d0
Sofia Alfaro <alfaros.sofia@gmail.com>
2024-05-21 17:55:09 -0500

    feat: Token created

---
+++ src/main/java/com/chapaTuBus/webService/userAccount/domain/model/aggregates/Token.java
@@ -0,0 +1,34 @@
+package com.chapaTuBus.webService.userAccount.domain.model.aggregates;
+
+import com.chapaTuBus.webService.userAccount.domain.model.valueobjects.TokenTypes;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    public Long id;
+
+    @Column(unique = true)
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenTypes tokenType =TokenTypes.BEARER;
+
+    public boolean revoked;
+
+    public boolean expired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", referencedColumnName = "id")
+    public User user;
+}
